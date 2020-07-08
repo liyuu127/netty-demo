b15ae49339ce2864bb96facf6fff06495e185098
liyu <1170667320@qq.com>
2020-07-08 18:07:09 +0800

    nio buffer

--- netty-sgu/src/test/java/cn/liyu/base/nio/BufferTest.java
+++ netty-sgu/src/test/java/cn/liyu/base/nio/BufferTest.java
@@ -1,30 +1,33 @@
 package cn.liyu.base.nio;
 
+import org.junit.Test;
+
 import java.nio.IntBuffer;
 
 /**
  * @author liyu
- * @date 2020/7/8 11:52
+ * @date 2020/7/8 15:44
  * @description
  */
 public class BufferTest {
 
-    public static void main(String[] args) {
-
-
+    @Test
+    public void intBuffer_simple_test() {
         //创建一个Buffer, 大小为 5, 即可以存放5个int
         IntBuffer intBuffer = IntBuffer.allocate(5);
         for (int i = 0; i < intBuffer.capacity(); i++) {
-            intBuffer.put(i+10);
+            intBuffer.put(i + 10);
         }
 
         //如何从buffer读取数据
         // *将buffer转换，读写切换
+        //        limit = position;
+        //        position = 0;
+        //        mark = -1;
         intBuffer.flip();
 
         while (intBuffer.hasRemaining()) {
             System.out.println("intBuffer.get() = " + intBuffer.get());
         }
-
     }
 }
